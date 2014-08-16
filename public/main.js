d224ba404d24a15f8f45287c20e213b8c5909bb0
Curtis H <ch@curtish.me>
2014-08-16 13:30:20 -0400

    Setting up ajax of images into page

--- public/main.js
+++ public/main.js
@@ -1,1 +1,26 @@
+function setupHandlers(container) {
+    container.find('img').click(function() {
+        
+    });
+}
 
+function getImages() {
+    $.get('/memes', function(images) {
+        if(images.length > 0) {
+            var imageArea = $(".imageArea");
+            imageArea.empty();
+            for(i in images) {
+                if(images.hasOwnProperty(i)) {
+                    var image = images[i];
+                    imageArea.append('<div class="imgContainer col-xs-12 col-md-4"><img src="/memes/'+image._id+'" /></div>');
+                }
+            }
+            
+            setupHandlers(imageArea);
+        }
+    });
+}
+
+$(function(){
+    getImages();
+});
